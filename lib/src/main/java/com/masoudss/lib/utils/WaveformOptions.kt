78181509cdb69e94d1d7f88fcae5f9f96c98d38b
lincollincol <andriy.serb1@gmail.com>
2021-08-15 18:25:39 +0300

    Update WaveformOptions.kt

--- lib/src/main/java/com/masoudss/lib/utils/WaveformOptions.kt
+++ lib/src/main/java/com/masoudss/lib/utils/WaveformOptions.kt
@@ -4,21 +4,20 @@ import android.content.Context
 import linc.com.amplituda.Amplituda
 import linc.com.amplituda.AmplitudaProcessingOutput
 import linc.com.amplituda.exceptions.AmplitudaException
-import java.io.File
 
 internal object WaveformOptions {
 
     @JvmStatic
     fun getSampleFrom(context: Context, pathOrUrl: String, onSuccess: (IntArray) -> Unit) {
-        handleAmplitudaOutput<String>(Amplituda(context).processAudio(pathOrUrl), onSuccess)
+        handleAmplitudaOutput(Amplituda(context).processAudio(pathOrUrl), onSuccess)
     }
 
     @JvmStatic
     fun getSampleFrom(context: Context, resource: Int, onSuccess: (IntArray) -> Unit) {
-        handleAmplitudaOutput<Int>(Amplituda(context).processAudio(resource), onSuccess)
+        handleAmplitudaOutput(Amplituda(context).processAudio(resource), onSuccess)
     }
 
-    private fun <T> handleAmplitudaOutput(
+    private fun handleAmplitudaOutput(
         amplitudaOutput: AmplitudaProcessingOutput<*>,
         onSuccess: (IntArray) -> Unit
     ) {
